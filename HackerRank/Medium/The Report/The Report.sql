bfe6a2e2fe63eafda2da0e33536df837fb4d9ca1
Shravani Reddy Voddula <124631598+ShravaniVoddula@users.noreply.github.com>
2024-07-22 19:06:25 -0500

    Update The Report.sql

--- HackerRank/Medium/The Report/The Report.sql
+++ HackerRank/Medium/The Report/The Report.sql
@@ -1,1 +1,12 @@
+select 
+    (case
+         when Grade <8 then null
+         WHEN Grade >=8 then Name
+          end) AS Name, Grade,Marks
+from(
 
+
+select T1.Name,T2.Grade AS Grade,T1.Marks
+from  Students T1 JOIN Grades  T2 ON T1.Marks between T2.Min_Mark AND T2.Max_mark
+    ) AS T12
+order by Grade desc,Name,Marks  asc
